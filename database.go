09396c20f520bedcc66234c3c124183d20e684e1
Milo Mordaunt <milomord@gmail.com>
2024-03-15 14:16:05 -0400

    Attempt to use fly pg

--- database.go
+++ database.go
@@ -4,23 +4,23 @@ import (
     "database/sql"
     "fmt"
     "log"
+	"os"
 
     _ "github.com/lib/pq"
 )
 
 var Db *sql.DB
 
-const (
-    host     = "localhost"
-    port     = 5432
-    user     = "dustapi"
-    password = "dustapi"
-    dbname   = "dust"
-)
+func getDbUrl() string {
+	if value, ok := os.LookupEnv("DATABASE_URL"); ok {
+		return value
+	}
+
+	return "postgres://dustapi:dustapi@localhost:5432/dust_server?sslmode=disable"
+}
 
 func ConnectDB() {
-    connString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname = %s sslmode=disable", host, port, user, password, dbname)
-    db, err := sql.Open("postgres", connString)
+    db, err := sql.Open("postgres", getDbUrl())
     if err != nil {
         log.Printf("failed to connect to database: %v", err)
 		panic(err)
